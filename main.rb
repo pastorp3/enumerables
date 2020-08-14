eea87a37e79ee2707b11e77c46e729b238561dc1
atenaiis <attennaisc@gmail.com>
2020-08-14 10:09:43 -0500

    added my inject and multipy els

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 module Enumerable
+
   def my_each
     return dup unless block_given?
 
@@ -8,8 +9,7 @@ module Enumerable
       i += 1
     end
   end
-  [1, 2, 3].my_each { |num| puts num }
-
+  
   def my_each_with_index
     return dup unless block_given?
 
@@ -19,7 +19,6 @@ module Enumerable
       i += 1
     end
   end
-  [8, 9, 10].my_each_with_index { |num, idx| puts "num is #{num} at index #{idx}" }
 
   def my_select
     return dup unless block_given?
@@ -58,7 +57,30 @@ module Enumerable
     end
     new_array
   end
+
+
+def my_inject( *input)
+
+  return enum_for unless block_given? 
+  
+  input = input.to_a  
+  argument = input + self
+  return nil if argument.length.zero?
+  return argument[0] if argument.length == 1
+
+  output = argument[0]
+  argument[1..argument.length - 1].my_each { |num| output = yield(output, num) }
+  output
+
 end
+def multiply_els(input)
+  input.my_inject {|k,n| k*n}
+  end
+end
+
+
+[1, 2, 3].my_each { |num| puts num }
+[8, 9, 10].my_each_with_index { |num, idx| puts "num is #{num} at index #{idx}" }
 
 p [3, 5, 9].my_select { |num| num.even? }
 p [0, 1, 2, 3, 4, 5, 6].my_all? { |x| x >= 1 }
@@ -66,3 +88,5 @@ p [1, 2, 3, 4, 5, 6, 7].my_any? { |x| x > 7 }
 p [1, 2, 3, 4, 5, 6, 7].my_none? { |x| x >= 7 }
 p [2, 2, 2, 2, 2, 3, 4].my_count { |x| x == 2 }
 p [2, 4, 5].my_map { |x| x * 2 }
+p ([5,6,10]).my_inject { |sum, n| sum + n }   
+p multiply_els([2,4,5])
