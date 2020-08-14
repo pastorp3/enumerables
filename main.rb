2a8c22f407629e428868ef57a1aaaef546719543
Jose <pastorpedraza3@gmail.com>
2020-08-14 10:18:12 -0500

    fix code errors

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 module Enumerable
-
   def my_each
     return dup unless block_given?
 
@@ -9,7 +8,7 @@ module Enumerable
       i += 1
     end
   end
-  
+
   def my_each_with_index
     return dup unless block_given?
 
@@ -21,11 +20,9 @@ module Enumerable
   end
 
   def my_select
-    return dup unless block_given?
-
-    new_array = []
-    my_each { |num| new_array << num if yield(num) }
-    new_array
+    array = []
+    my_each { |x| array << x if yield(x) }
+    array
   end
 
   def my_all?
@@ -58,35 +55,33 @@ module Enumerable
     new_array
   end
 
+  def my_inject(*input)
+    return enum_for unless block_given?
 
-def my_inject( *input)
+    input = input.to_a
+    argument = input + self
+    return nil if argument.length.zero?
+    return argument[0] if argument.length == 1
 
-  return enum_for unless block_given? 
-  
-  input = input.to_a  
-  argument = input + self
-  return nil if argument.length.zero?
-  return argument[0] if argument.length == 1
-
-  output = argument[0]
-  argument[1..argument.length - 1].my_each { |num| output = yield(output, num) }
-  output
-
-end
-def multiply_els(input)
-  input.my_inject {|k,n| k*n}
+    output = argument[0]
+    argument[1..argument.length - 1].my_each { |num| output = yield(output, num) }
+    output
   end
 end
 
+def multiply_els(input)
+  input.my_inject { |k, n| k * n }
+end
+
+# Test
 
 [1, 2, 3].my_each { |num| puts num }
 [8, 9, 10].my_each_with_index { |num, idx| puts "num is #{num} at index #{idx}" }
-
-p [3, 5, 9].my_select { |num| num.even? }
-p [0, 1, 2, 3, 4, 5, 6].my_all? { |x| x >= 1 }
-p [1, 2, 3, 4, 5, 6, 7].my_any? { |x| x > 7 }
-p [1, 2, 3, 4, 5, 6, 7].my_none? { |x| x >= 7 }
-p [2, 2, 2, 2, 2, 3, 4].my_count { |x| x == 2 }
-p [2, 4, 5].my_map { |x| x * 2 }
-p ([5,6,10]).my_inject { |sum, n| sum + n }   
-p multiply_els([2,4,5])
+p[1, 2, 3, 4, 5, 6].my_select { |x| x.is_a?(Integer) }
+p[0, 1, 2, 3, 4, 5, 6].my_all? { |x| x >= 1 }
+p[1, 2, 3, 4, 5, 6, 7].my_any? { |x| x > 7 }
+p[1, 2, 3, 4, 5, 6, 7].my_none? { |x| x >= 7 }
+p[2, 2, 2, 2, 2, 3, 4].my_count { |x| x == 2 }
+p[2, 4, 5].my_map { |x| x * 2 }
+p[5, 6, 10].my_inject { |sum, n| sum + n }
+p multiply_els([2, 4, 5])
