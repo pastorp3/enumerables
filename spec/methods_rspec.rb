54207af3f99bc4774aca080de6c7fa083f2a5956
Jose <pastorpedraza3@gmail.com>
2020-08-27 10:43:07 -0500

    add all my_all? test

--- spec/methods_rspec.rb
+++ spec/methods_rspec.rb
@@ -13,7 +13,19 @@ public
         self
       end
 
- 
+      def my_all_option1(input, arr)
+        count = 0
+        arr.my_each do |x|
+          if input.is_a?(Integer)
+            count += 1 if x == input
+          elsif input.is_a?(Regexp)
+            count += 1 unless (x =~ input).nil?
+          elsif input.is_a?(Class)
+            count += 1 if x.is_a?(input)
+          end
+        end
+        count
+      end
 
     def my_all?(input = nil)
         arr = to_a
@@ -49,13 +61,36 @@ public
     end
 
     describe'#my_all?' do 
-      it 'asdsa' do
+      it 'my_all? test with string = true' do
        arr = ['ant' ,'bear' ,'cat']
         expect((arr).my_all? { |word| word.length >= 3 }).to eql(true)
-
-        expect((arr).my_all? { |word| word.length >= 4 }).to eql(false)
-
       end
+      it 'my_all? test with string = false' do
+        arr = ['ant' ,'bear' ,'cat']
+         expect((arr).my_all? { |word| word.length >= 4 }).to eql(false)
+ 
+       end
+       it 'my_all? test with regex' do
+        arr = ['ant' ,'bear' ,'cat']
+         expect((arr).my_all?(/t/)).to eql(false)
+ 
+       end
+       it 'my_all? test with class' do
+        arr = [1, 2i, 3.14]
+         expect((arr).my_all?(Numeric)).to eql(true)
+ 
+       end
+       it 'my_all? test with nil elements' do
+        arr = [nil ,false , 99]
+         expect((arr).my_all?).to eql(false)
+ 
+       end
+       it 'my_all? test with no elements' do
+        arr = []
+         expect((arr).my_all?).to eql(true)
+ 
+       end
+
     end
 
 
